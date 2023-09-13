ea12e04c117d706b81e9f51c1691fd16bd390b96
João Carlos <joao.carlos_25@hotmail.com>
2023-09-13 20:27:30 -0300

    feat: Construindo componente xml-upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { XmlUploadComponent } from './components/xml-upload/xml-upload.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,14 +6,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+
+const modulesAngularMaterial = [
+  MatCardModule,
+  MatListModule,
+  MatToolbarModule,
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XmlUploadComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    modulesAngularMaterial
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/xml-upload/xml-upload.component.ts
+++ src/app/components/xml-upload/xml-upload.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { XmlUploadService } from 'src/app/services/xml-upload.service';
 
 @Component({
   selector: 'app-xml-upload',
   templateUrl: './xml-upload.component.html',
   styleUrls: ['./xml-upload.component.css']
 })
-export class XmlUploadComponent {
+export class XmlUploadComponent{
+
+  constructor(xmlUploadService: XmlUploadService){
+
+  }
 
 }
