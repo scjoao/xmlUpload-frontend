c475ce19c4f44bcebf2d2cdffb93c6e2b503e59b
João Carlos <joao.carlos_25@hotmail.com>
2023-09-13 21:25:15 -0300

    feat: Construindo service xml-upload

--- src/app/services/xml-upload.service.ts
+++ src/app/services/xml-upload.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class XmlUploadService {
 
-  constructor() { }
+  private baseXmlUrl = "http://localhost:8080";
+
+  constructor(private http: HttpClient) {
+  }
+
+  public xmlUpload(xmlFile: File): Observable<HttpEvent<any>> {
+
+    const formData: FormData = new FormData();
+    formData.append('xmlFile', xmlFile);
+
+    const request = new HttpRequest("POST", `${this.baseXmlUrl}/xmlUpload`, formData, {
+      reportProgress: true,
+      responseType: 'json'
+    });
+
+    return this.http.request(request);
+  }
+
+  public getXmlFiles(): Observable<any> {
+    return this.http.get(`${this.baseXmlUrl}/xmlFiles`);
+  }
 }
